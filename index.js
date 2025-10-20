d7c454747af0ca042bb91a7929ef14b281789717
AyresGaia <tigermaster3002@gmail.com>
2025-10-20 17:38:50 +0530

    Secound

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello Archduke 👑 — CI/CD says hi!'));
+app.get('/', (req, res) => res.send('Hello Archduke 👑 — CI/CD says hi! Its time to change the World'));
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
